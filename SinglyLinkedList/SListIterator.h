9adefcf1bf11a920594051f30b191781f5d15c60
Derek Flood <floodd@dkit.ie>
2024-10-10 12:34:52 +0100

    Updated solutions to include display, swap pairs and remove duplicates.

--- SinglyLinkedList/SListIterator.h
+++ SinglyLinkedList/SListIterator.h
@@ -1,7 +1,9 @@
 template <class T> class SList;
 template <class T> class SListIterator; 
+template <class T> class SListNode;
 #pragma once
 #include "SList.h"
+#include "SListNode.h"
 
 template <class T>
 class SListIterator
@@ -15,7 +17,32 @@ public:
 	void start();
 	bool isValid();
 	T& item();
+	T& operator*();
+	SListIterator<T> operator++();
+	SListIterator<T> operator++(int i);
+
 };
+
+
+template <class T>
+T& SListIterator<T>::operator*()
+{
+	return currentNode->getData();
+}
+template <class T>
+SListIterator<T> SListIterator<T>::operator++()
+{
+	currentNode = currentNode->getNext();
+	return *this;
+}
+template <class T>
+SListIterator<T> SListIterator<T>::operator++(int i)
+{
+	SListIterator<T> temp(*this);
+	currentNode = currentNode->getNext();
+	return temp;
+}
+
 
 template <class T>
 SListIterator<T>::SListIterator(SList<T> *l, SListNode<T> *n)
--- SinglyLinkedList/SinglyLinkedList.cpp
+++ SinglyLinkedList/SinglyLinkedList.cpp
@@ -1,10 +1,85 @@
-// SinglyLinkedList.cpp : Defines the entry point for the console application.
-//
+#include <iostream>
+#include "SList.h"
 
+using namespace std;
+template <class T>
+void removeDuplicates(SList<T>& lst)
+{
+    SListIterator<T> outer = lst.getIterator();
+    while (outer.isValid())
+    {
+        SListIterator<T> inner(outer);
+        inner++;
+        while (inner.isValid())
+        {
+            if (*inner == *outer)
+            {
+                SListIterator<T> temp(inner);
+                temp++;
+                lst.remove(inner);
+                inner = temp;
+            }
 
+            else
+            {
+                inner++;
+            }
+        }
+        outer++;
+    }
+}
+template <class T>
+void swapPairs(SList<T>& lst)
+{
+    SListNode<T>* st = lst.head;
+    SListNode<T>* prev=nullptr;
+    while (st != lst.tail && st != nullptr)
+    {
+        SListNode<T>* a =prev;
+        SListNode<T>* b= st;
+        SListNode<T>* c = st->getNext();
+        SListNode<T>* d = st->getNext()->getNext();
+        if (a != nullptr)
+            a->setNext(c);
 
+        b->setNext(d);
+        c->setNext(b);
+        if (b == lst.head)
+            lst.head = c;
+        if (c == lst.tail)
+            lst.tail = b;
+
+        st = d;
+        prev = b;
+
+    }
+}
+template <class T>
+void display(SList<T>& lst)
+{
+    SListIterator<T> it = lst.getIterator();
+    while (it.isValid())
+    {
+        cout << *it <<" " ;
+        it++;
+    }
+    cout << endl;
+}
 int main()
 {
+    SList<int> lst;
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int x = 1; x <= i; x++)
+        {
+            lst.append(i);
+        }
+    }
+    display(lst);
+    removeDuplicates(lst);
+    display(lst);
+    swapPairs(lst);
+    display(lst);
     return 0;
 }
 
